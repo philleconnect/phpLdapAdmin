f86c83ed87b84040835042520effe220413f5728
Johannes Kreutz <jkm@jkm-marburg.de>
2020-08-12 11:11:12 +0200

    Move to v2 plugin structure

--- phpldapadmin/entrypoint.sh
+++ phpldapadmin/entrypoint.sh
@@ -18,13 +18,10 @@ if [[ -z "$SLAPD_DOMAIN1" ]]; then
 fi
 
 # set the environment settings for slapd:
-sed -i "s|$servers->setValue('server','host','127.0.0.1');|$servers->setValue('server','host','$SLAPD_DOMAIN1.$SLAPD_DOMAIN0');|g" /etc/phpldapadmin/config.php
+sed -i "s|$servers->setValue('server','host','127.0.0.1');|$servers->setValue('server','host','ldap');|g" /etc/phpldapadmin/config.php
 sed -i "s|$servers->setValue('server','base',array('dc=example,dc=com'));|$servers->setValue('server','base',array('dc=$SLAPD_DOMAIN1,dc=$SLAPD_DOMAIN0'));|g" /etc/phpldapadmin/config.php
 sed -i "s|$servers->setValue('login','bind_id','cn=admin,dc=example,dc=com');|$servers->setValue('login','bind_id','cn=admin,dc=$SLAPD_DOMAIN1,dc=$SLAPD_DOMAIN0');|g" /etc/phpldapadmin/config.php
 sed -i "s|// $config->custom->appearance['hide_template_warning'] = false;|$config->custom->appearance['hide_template_warning'] = true;|g" /etc/phpldapadmin/config.php
-
-# check connection, TODO: raus werfen!
-#ldapsearch -x -h slapd -b "cn=admin,dc=$SLAPD_DOMAIN1,dc=$SLAPD_DOMAIN0"
 
 #chown -R openldap:openldap /var/lib/ldap/ /var/run/slapd/
 /usr/sbin/apache2ctl -D FOREGROUND
